d3dd3e7497d09fd4f42a1bd2553081476ce5bfbe
bule-ttt <taiseisugawara4@gmail.com>
2026-03-06 18:00:06 +0900

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,11 @@
+fetch("header.html")
+.then(response=>responce.text())
+.then(data=>{
+ document.getElementById("header").inner
+});
+
+fetch("navi.html")
+.then(response=>responce.text())
+.then(data=>{
+ document.getElementById("navi").inner
+});
