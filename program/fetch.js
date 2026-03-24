770fe20f005bf2a5ca5335d0aa857157cb9c4a4c
bule-ttt <taiseisugawara4@gmail.com>
2026-03-24 17:59:01 +0900

    Update fetch.js

--- program/fetch.js
+++ program/fetch.js
@@ -4,7 +4,7 @@ fetch("header.html")
  document.getElementById("header").innerHTML=data;
 });
 
-fetch("program/navi.html")
+fetch("https://blue-ttt.github.io/plot/program/navi.html")
 .then(response=>response.text())
 .then(data=>{
  document.getElementById("navi").innerHTML=data;
