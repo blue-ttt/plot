582aaeeb3167999526453f4cc1d93b4dd97cafda
bule-ttt <taiseisugawara4@gmail.com>
2026-04-22 21:01:38 +0900

    Create i18n.js

---
+++ program/i18n.js
@@ -0,0 +1,23 @@
+const language = navigator.language;
+const elements = document.querySelectorAll(".i18n");
+
+if(language.startsWith('ja'))
+{
+ elements.forEach((el) => {
+    const text = el.textContent;
+    if (text.includes("|")) {
+      const parts = text.split("|");
+      el.textContent = parts[0]; // 前側の日本語を表示
+    }
+ })
+}
+else
+{
+  elements.forEach((el) => {
+    const text = el.textContent;
+    if (text.includes("|")) {
+      const parts = text.split("|");
+      el.textContent = parts[1]; // 後ろ側の英語を表示
+    }
+  })
+}
