e72f0efed6bbd57134c682dc19b93f8cbc02090c
bule-ttt <taiseisugawara4@gmail.com>
2026-04-24 17:11:21 +0900

    Update function.js

--- program/core/function.js
+++ program/core/function.js
@@ -1,1 +1,36 @@
-
+// --- ボタンの関数 ---
+  function ShareDisplay(){ /* 共有 */
+   const target = document.querySelector('.share');
+   target.style.display = "block";
+  };
+  function ShareClose(){ /* 共有閉じる */
+   const target = document.querySelector('.share');
+   target.style.display = "none";
+  };
+  function SettingDisplay(){ /* 設定 */
+   const target = document.querySelector('.mini-setting');
+   target.style.display = "block";
+  };
+  function SettingClose(){ /* 設定閉じる */
+   const target = document.querySelector('.mini-setting');
+   target.style.display = "none";
+  };
+  function HelpDisplay(){ /* ヘルプ */
+   const target = document.querySelector(".mini-study");
+   target.style.display = "block";
+   OverlayIsDisplay();
+  };
+  function StudyClose(){ /* ヘルプ閉じる */
+   const target = document.querySelector(".mini-study");
+   target.style.display = "none";
+   OverlayIsNone();
+  };
+// --- オーバーレイの関数 ---
+  function OverlayIsDisplay(){
+   const target = document.querySelector(".overlay");
+   target.style.display = "block";
+  };
+  function OverlayIsNone(){
+   const target = document.querySelector(".overlay");
+   target.style.display = "none";
+  };
