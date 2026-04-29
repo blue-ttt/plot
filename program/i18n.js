c23121ba78bac9cd30290cee628ed17c22c2651f
bule-ttt <taiseisugawara4@gmail.com>
2026-04-29 17:54:55 +0900

    Update i18n.js

--- program/i18n.js
+++ program/i18n.js
@@ -8,9 +8,17 @@ if(language.startsWith('ja')) {
     const text = el.textContent;
     if (text.includes("|")) {
       const parts = text.split("|");
-      el.textContent = parts[0]; // 前側の日本語を表示
+      el.textContent = parts[0]; /* 前側の日本語を表示 */
     }
- })
+ 
+ const tooltipText = el.getAttribute("data-tooltip"); //要素（el）の data-tooltip 属性の中身を読み取る
+ if (tooltipText && tooltipText.includes("|")) { // data-tooltip が設定されているか？文字列の中に | が含まれているか？をチェック
+  const parts = tooltipText.split("|");
+  el.setAttribute("data-tooltip", parts[0]);
+ }
+  
+  }) //elements.forEach((el)ここまで
+ 
 }
 else
 {
@@ -18,7 +26,7 @@ else
     const text = el.textContent;
     if (text.includes("|")) {
       const parts = text.split("|");
-      el.textContent = parts[1]; // 後ろ側の英語を表示
+      el.textContent = parts[1]; /* 後ろ側の英語を表示 */
     }
   });
 };
