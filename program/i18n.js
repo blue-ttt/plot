6511d1a0b5157df66718ae32be2f466e97e69170
bule-ttt <taiseisugawara4@gmail.com>
2026-04-30 17:36:19 +0900

    Update i18n.js

--- program/i18n.js
+++ program/i18n.js
@@ -8,7 +8,7 @@ if(language.startsWith('ja')) {
     const text = el.textContent;
     if (text.includes("|")) {
       const parts = text.split("|");
-      el.textContent = parts[1]; /* 前側の日本語を表示 */
+      el.textContent = parts[0]; /* 前側の日本語を表示 */
     }
  // ツールチップ
  const tooltipText = el.getAttribute("data-tooltip"); //要素（el）の data-tooltip 属性の中身を読み取る
@@ -26,7 +26,7 @@ else
     const text = el.textContent;
     if (text.includes("|")) {
       const parts = text.split("|");
-      el.textContent = parts[0]; /* 後ろ側の英語を表示 */
+      el.textContent = parts[1]; /* 後ろ側の英語を表示 */
     }
     // ツールチップ
  const tooltipText = el.getAttribute("data-tooltip"); //要素（el）の data-tooltip 属性の中身を読み取る
