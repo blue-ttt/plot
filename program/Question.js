55ee3c1f5b10a47c0ff884897b1b40586f97a0a5
bule-ttt <taiseisugawara4@gmail.com>
2026-05-02 21:20:34 +0900

    Update Question.js

--- program/Question.js
+++ program/Question.js
@@ -1,3 +1,21 @@
-function YesOrNo(text){
-  
+function YesOrNo(title , text , yesName , noName){
+  const html = `
+  <div class="QuestionCade">
+    <h2>${title}</h2>
+    <p>${text}</p>
+    <button>${yesName}</button>
+    <button>${noName}</button>
+  </div>
+  `;
+}
+function doDelete(){
+  const html = `
+  <div class="QuestionCade">
+    <h2>削除しますか？</h2>
+    <h3>警告</h3>
+    <p>これにより指定されたものは削除されます。取り消しは出来ません</p>
+    <button>はい正気です</button>
+    <button>キャンセル</button>
+  </div>
+  `;
 }
