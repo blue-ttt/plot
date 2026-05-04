91f124f5f19bcf06316b6c4869a8cfe91ff531c0
bule-ttt <taiseisugawara4@gmail.com>
2026-05-04 17:27:04 +0900

    Update Question.js

--- program/Question.js
+++ program/Question.js
@@ -7,6 +7,6 @@ function YesOrNo(title , text , yesName , noName){
     <button class="btn-QuestionCancel">${noName}</button>
   </div>
   `;
-  const YesOrNo = document.getElementById('QuestionYesOrNo');/* <div id="YesOrNo"></div>に表示 */
-  targetElement.innerHTML = html;
+  const YesOrNoData = document.getElementById('QuestionYesOrNo');/* <div id="YesOrNo"></div>に表示 */
+  YesOrNoData.innerHTML = html;
 }
