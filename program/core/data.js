9ae4c6f6c1d57241eb31352712e3bc27bcbdd7b7
bule-ttt <taiseisugawara4@gmail.com>
2026-05-14 17:58:58 +0900

    Update data.js

--- program/core/data.js
+++ program/core/data.js
@@ -1,7 +1,9 @@
+let PlotStyleType = 
+    
 let plot_edit_data_head = {
-    info: 'This_data_is_the_work_of_a_Plot_user', type: value_plot_type,
-},
+    info: 'This_data_is_the_work_of_a_Plot_user', type: PlotStyleType,
+};
   
 let plot_edit_data_body = [
-  {}
-]
+
+];
