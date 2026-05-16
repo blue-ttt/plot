5f88744b6beed55aed151bb2994018b22ff4b1dc
bule-ttt <taiseisugawara4@gmail.com>
2026-05-16 21:22:07 +0900

    Update data.js

--- program/core/data.js
+++ program/core/data.js
@@ -2,7 +2,7 @@ localStorage.setItem('recently_plot_style_type', 'ノート');
 
 // 変数を設定
 let PlotStyleType = localStorage.getItem('recently_plot_style_type');
-    console.log(PlotStyleType);
+    console.log('plotのスタイルタイプは' + PlotStyleType);
 
 let plot_edit_data_head = {
     info: 'This_data_is_the_work_of_a_Plot_user', type: PlotStyleType,
