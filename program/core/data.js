8fd4c92e495adc74b53ba0fa0bc275fea1f7500c
bule-ttt <taiseisugawara4@gmail.com>
2026-05-18 11:38:31 +0900

    Update data.js

--- program/core/data.js
+++ program/core/data.js
@@ -1,6 +1,6 @@
 localStorage.setItem('recently_plot_style_type', 'ノート');
 
-// 変数を設定
+// --- 変数を設定 ---
 let PlotStyleType = localStorage.getItem('recently_plot_style_type');
     console.log('plotのスタイルタイプは' + PlotStyleType);
 
@@ -9,5 +9,19 @@ let plot_edit_data_head = {
 };
   
 let plot_edit_data_body = [
-
+    {
+        id: '10036',
+        x:  '0',
+        y: '0',
+        size: '1.0',
+        type: 'note',
+        attrs: {
+            color: '#FFF9E6',
+            text_color: '#000',
+            width: '100px',
+            height: '120px',
+        }
+    }
 ];
+
+// --- 編集画面への反映 ---
