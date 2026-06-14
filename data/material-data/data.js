70b75185f5022adcb513d54bafdec2af7f3a33d8
bule-ttt <taiseisugawara4@gmail.com>
2026-06-14 17:12:13 +0900

    Update data.js

--- data/material-data/data.js
+++ data/material-data/data.js
@@ -1,3 +1,3 @@
 const plot_MaterialDecoration_data = [
-  { name: 'Plot_kun_normal', img: 'https://blue-ttt.github.io/plot/data/edit-material-svg/plot-character.svg',},
+  { name: 'Plot_kun_normal', img: 'https://blue-ttt.github.io/plot/data/edit-material-svg/plot-character.svg', other_name: 'プロットくん　plot公式 キャラクター', },
 ];
