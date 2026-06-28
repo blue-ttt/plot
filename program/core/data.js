bebf884488286f32642448b012a1cafde3e2d644
bule-ttt <taiseisugawara4@gmail.com>
2026-06-28 21:03:37 +0900

    Update data.js

--- program/core/data.js
+++ program/core/data.js
@@ -36,17 +36,23 @@ function edit_update () {
         console.log(`ID:${item.id}, X座標:${item.x}, Y座標:${item.y},color:${item.attrs.color}`);
         
         let input_material_html_data = ''; /* ifの前に変数を作っとく(if内だとinnerHTMLが見つけられないから) */
-        if (item.type === 'note'){
-           input_material_html_data = `
-             <div class="sticky-note" style="background-color:${item.attrs.color};">
-                  <textarea placeholder="メモを入力..." maxlength="200" name="note" style="color:${item.attrs.text_color};"></textarea>
-             </div>
-        `;
-        };
-
-        const input_material_html = document.getElementById('canvas');
-        input_material_html.innerHTML = input_material_html_data;
         
+        // 付箋の部分
+        if (item.type === 'note'){
+
+            const noteWrapper = document.createElement('div');/* 文字列から実際のDOM要素（div）を作成するためのラッパーを作る(座標を変えるなどは文字だとできず、DOM要素にする必要があるから) */
+            
+            // 付箋のhtmlを上のに流し込む
+            noteWrapper.innerHTML = `
+                <div class="sticky-note" style="background-color:${item.attrs.color}; width:${item.attrs.width}; height:${item.attrs.width};">
+                  <textarea placeholder="メモを入力..." maxlength="200" name="note" style="color:${item.attrs.text_color};"></textarea>
+                 </div>
+        `;
+            const noteElement = noteWrapper.querySelector('.sticky-note');
+            noteElement.style.transform = `translate(${item.x}px,${item.y}px,)`;
+
+            canvas.appendChild(noteElement);/* #canvasに「追加」する(innerHTMLだと全て上書きしてしまうから) */
+        };
     });
 };
 window.onload = function() {
