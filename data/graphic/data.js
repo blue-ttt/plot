487a05d37b32067c053e709451c99d6f68432463
bule-ttt <taiseisugawara4@gmail.com>
2026-07-19 17:03:59 +0900

    作成

---
+++ data/graphic/data.js
@@ -0,0 +1,15 @@
+const graphic_circle =
+`<svg width="100%" height="100%" viewBox="0 0 2481 2481" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(1,0,0,1,-18,-2920)"><g transform="matrix(1,0,0,1,-10805.535433,2920)"><g transform="matrix(1.547296,0,0,1.547296,9619.739401,-5786.88581)"><circle cx="1579.5" cy="4541.5" r="801.5"/></g></g></g>
+</svg>`;
+
+const graphic_square =
+`<svg width="100%" height="100%" viewBox="0 0 2481 2481" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(1,0,0,1,-2693,-2920)">
+        <g transform="matrix(1,0,0,1,-8130.535433,2920)">
+            <g transform="matrix(1.803865,0,0,2,5965.725841,-5840)">
+                <rect x="2693" y="2920" width="1375" height="1240.157"/>
+            </g>
+        </g>
+    </g>
+</svg>`;
